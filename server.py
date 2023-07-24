9402f2eb8ae12d4f3706f4fb1faf0b6d67075781
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2023-07-24 12:53:12 +0800

    Update server.py

--- server.py
+++ server.py
@@ -16,14 +16,14 @@ urls = (
 )
 
 headers = {
-        'Host': 'yierco.slack.com',
-        'Cookie': '',
+        'Host': '139wokao.slack.com',
+        'Cookie': 'b=97cf412a5913ef6babd6e2d49526317a; _gcl_au=1.1.350294721.1689900899; OptanonAlertBoxClosed=2023-07-21T01:05:01.705Z; _lc2_fpi=e00b11ac9c9b--01h5tznba97gm4qfcace6wjt4s; _cs_c=1; d=xoxd-iM9msWLuwQXUkNcO3jc5v%2BxWqAQVoMsq694hWsOIRmeGSZjHGBjU7EX%2FVZFnpZUg5aPQZ17N6UzsklUoViepJzPaiz9DC0coGG8KhdAyv%2FKqUGopgCxD2oyshXy45%2BKcmEcf3SxJ6VpdzfO2w2GXE5RMbkvTtG9OtszUwDPmtJWS24Pc3A1ZZtnsdQ%3D%3D; lc=1689901519; tz=480; d-s=1690169834; x=97cf412a5913ef6babd6e2d49526317a.1690169834; _cs_mk_ga=0.6672402806626914_1690169840452; DriftPlaybook=B; _ga_QTJQME5M5D=GS1.1.1690169840.2.0.1690169840.60.0.0; _ga=GA1.2.165405858.1689901509; _gid=GA1.2.1010259680.1690169841; _cs_cvars=%7B%221%22%3A%5B%22Visitor%20ID%22%2C%2297cf412a5913ef6babd6e2d49526317a%22%5D%2C%222%22%3A%5B%22Is%20Signed%20In%22%2C%22true%22%5D%2C%223%22%3A%5B%22URL%20Path%22%2C%22%2Fintl%2Fzh-cn%2F%22%5D%2C%224%22%3A%5B%22Visitor%20Type%22%2C%22prospect%22%5D%7D; _cs_id=a46bce8e-3f15-a79a-cff6-d714ee0b3077.1689901510.2.1690169841.1690169841.1.1724065510294; _cs_s=1.0.0.1690171641982; _li_dcdm_c=.slack.com; __adroll_fpc=afd56902a678be4de8158818eb468875-1690169855403; __qca=P0-156302808-1690169841266; shown_ssb_redirect_page=1; __ar_v4=%7C4UHU5P4P3FESHLUMNBLWAU%3A20230723%3A1%7CQCM34G7NBZEHHATIFDIUBJ%3A20230723%3A1%7CK2HN2U4VSJGOVKC2WJLQNH%3A20230723%3A1; OptanonConsent=isGpcEnabled=0&datestamp=Mon+Jul+24+2023+11%3A39%3A19+GMT%2B0800+(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4)&version=202211.1.0&isIABGlobal=false&hosts=&consentId=f029ca60-b6cf-42dc-8942-fef002ae79dc&interactionCount=2&landingPath=NotLandingPage&groups=1%3A1%2C2%3A1%2C3%3A1%2C4%3A1&geolocation=CN%3BSC&AwaitingReconsent=false; shown_download_ssb_modal=1; show_download_ssb_banner=1; no_download_ssb_banner=1',
     }
 
-token=""
+token="xoxc-5586135847479-5613310679857-5638833143200-55aa0a1d75aa01e2b3ff00505c5789c5a232307a782f430f15d3cd1725a696a4"
 
-Claude_userid=""
-fangjian_id=""
+Claude_userid="U05J194KZR7"
+fangjian_id="D05HR3W314L"
 
 app = web.application(urls, globals())
 app.debug = False
@@ -73,7 +73,7 @@ class setsession:
         prompt=message
 
 
-        url_post_postMessage = 'https://yierco.slack.com/api/chat.postMessage'
+        url_post_postMessage = 'https://139wokao.slack.com/api/chat.postMessage'
 
         if(url_conversations_replies_huoqu_latest_reply_json_latest_reply2!=None):
             url_post_postMessage_file = {
@@ -156,7 +156,7 @@ class stream:
         # url_conversations_replies_huoqu_latest_reply_json_latest_reply2 = web.cookies().get('latest_reply')
         url_post_postMessage_r_json_thread_ts = web.cookies().get('thread_ts')
 
-        url_conversations_replies = 'https://yierco.slack.com/api/conversations.replies'
+        url_conversations_replies = 'https://139wokao.slack.com/api/conversations.replies'
 
 
         for i in range(120):
